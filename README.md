e5af7d5dbe448aa789c8f3a95077601830ecdb87
Lucas O'Donnell <lucas.odonnelll@gmail.com>
2021-05-22 18:59:37 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Making a small Library app for the Odin Project
---
+++ script.js
@@ -0,0 +1,77 @@
+class Book {
+	constructor(params) {
+		this.title = params.title;
+		this.author = params.author;
+		this.pages = params.pages;
+		this.read = params.read;
+	}
+
+	readStatus() {
+		this.read == true ? false : true;
+	} 
+}
+
+// Pulls the stored library and makes my Library**************************************** 
+
+let myLibrary = [];
+const storedLibrary = localStorage.getItem('myLibrary');
+if (storedLibrary) {
+	myLibrary = JSON.parse(storedLibrary).map((book) => new Book(book));
+}
+
+//**************************************************************************************
+
+const showModalButton = document.querySelector('[data-modal-target]');
+const hideModalButton = document.querySelector('[data-close-button]');
+const overlay = document.getElementById('overlay');
+const popUpModal = document.querySelector('[data-modal]');
+const bookForm = document.querySelector('[data-new-book-form]')
+
+//Buttons to open and close modal********************************************************
+showModalButton.addEventListener('click', () => {
+	const modal = document.querySelector(showModalButton.dataset.modalTarget);
+	showModal(modal);
+});
+
+hideModalButton.addEventListener('click', () => {
+	const modal = hideModalButton.closest(".modal");
+	hideModal(modal);
+});
+
+function showModal(modal) {
+	if (modal == null) return;
+	modal.classList.add('active');
+	overlay.classList.add('active');
+}
+
+function hideModal(modal) {
+	if (modal == null) return;
+	modal.classList.remove('active');
+	overlay.classList.remove('active');
+}
+//Submit Form***************************************************************************
+bookForm.addEventListener('submit', newBook);
+
+function newBook(e) {
+	e.preventDefault();
+	book = new Book({
+		title: document.querySelector('[name=title]').value,
+		author: document.querySelector('[name=author]').value,
+		pages: document.querySelector('[name=pages]').value,
+		read: document.querySelector('[name=read]').checked
+	})
+	addBookToLibrary(book);
+	hideModalOnSubmit();
+	this.reset();
+}
+
+function addBookToLibrary(book) {
+	myLibrary.push(book);
+	localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+}
+
+function hideModalOnSubmit() {
+	popUpModal.classList.remove('active');
+	overlay.classList.remove('active');
+}
+//**************************************************************************************
